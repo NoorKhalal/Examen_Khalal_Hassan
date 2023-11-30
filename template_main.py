74a3ba9759a02dc35a70d5b70a52b4cdd725b97b
Youssef Hassan <youssefhassan1211@gmail.com>
2023-11-30 11:23:56 +0100

    Update template_main.py with pca and umap

--- template_main.py
+++ template_main.py
@@ -3,52 +3,32 @@ from sklearn.metrics.cluster import normalized_mutual_info_score, adjusted_rand_
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
+from sklearn.decomposition import PCA
+import umap
+
+from sklearn.cluster import KMeans
 
 def dim_red(mat, p, method):
-    '''
-    Perform dimensionality reduction
-
-    Input:
-    -----
-        mat : NxM list 
-        p : number of dimensions to keep 
-    Output:
-    ------
-        red_mat : NxP list such that p<<m
-    '''
     if method=='ACP':
-        red_mat = mat[:,:p]
-        
-    elif method=='AFC':
-        red_mat = mat[:,:p]
-        
+      pca = PCA(n_components=p)
+      mat = pca.fit_transform(mat)
+      red_mat = mat[:,:p]
+
     elif method=='UMAP':
-        red_mat = mat[:,:p]
-        
+      reducer = umap.UMAP(n_components=p)
+      red_mat = reducer.fit_transform(mat)
+
     else:
         raise Exception("Please select one of the three methods : APC, AFC, UMAP")
-    
+
     return red_mat
 
 
 def clust(mat, k):
-    '''
-    Perform clustering
+  kmeans = KMeans(n_clusters=k, random_state=42)
+  kmeans.fit(mat)
+  return kmeans.labels_
 
-    Input:
-    -----
-        mat : input list 
-        k : number of cluster
-    Output:
-    ------
-        pred : list of predicted labels
-    '''
-    
-    pred = np.random.randint(k, size=len(corpus))
-    
-    return pred
-
-# import data
 ng20 = fetch_20newsgroups(subset='test')
 corpus = ng20.data[:2000]
 labels = ng20.target[:2000]
@@ -58,8 +38,7 @@ k = len(set(labels))
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 embeddings = model.encode(corpus)
 
-# Perform dimensionality reduction and clustering for each method
-methods = ['ACP', 'AFC', 'UMAP']
+methods = ['ACP','UMAP']
 for method in methods:
     # Perform dimensionality reduction
     red_emb = dim_red(embeddings, 20, method)
@@ -73,4 +52,3 @@ for method in methods:
 
     # Print results
     print(f'Method: {method}\nNMI: {nmi_score:.2f} \nARI: {ari_score:.2f}\n')
-
